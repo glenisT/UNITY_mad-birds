8bbc3174c012b79f0808f06dc995550653121092
User <glentivari@gmail.com>
2021-06-22 16:22:07 +0200

    added main menu(work in progress)

---
+++ Assets/TextSpawner.cs
@@ -0,0 +1,31 @@
+using System.Net.Mime;
+using System.Globalization;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextSpawner : MonoBehaviour
+{
+
+    private TextMeshPro _text;
+
+    void Awake()
+    {
+        _text = GetComponent<TextMeshPro>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
